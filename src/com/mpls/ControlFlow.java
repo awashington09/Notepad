6c769090b489653a695d6ef5c724bff6a7c08f92
Student <student@DC-TECH-19.local>
2020-10-09 16:39:43 -0500

    first commit

---
+++ src/com/mpls/ControlFlow.java
@@ -0,0 +1,31 @@
+package com.mpls;
+
+public class ControlFlow {
+
+    //field variables
+    boolean isSunny;
+    int degrees = 66;
+    boolean isCold;
+
+
+    public boolean isTrue(boolean myBool){
+        // local variables
+        int myHeight = 6
+
+
+        return myBool;
+
+    }
+  public boolean checkWeather(int degrees){
+        if (degrees > 65){
+            isSunny = true;
+        }else{
+            isSunny = false;
+
+        }
+        // this is referencing the variable declared. "talking about it again"
+        return isSunny = true
+}
+
+
+}
---
+++ src/com/mpls/Main.java
@@ -0,0 +1,28 @@
+package com.mpls;
+
+public class Main {
+    static byte myByte;
+    static int myInt;
+    static short myShort;
+    static double myDouble;
+    static float myFloat;
+    static char myChar;
+    static long myLong;
+    static boolean myBool;
+    static String myString;
+
+    
+    public static void main(String[] args) {
+        myByte = 1;
+        myInt = 4;
+        myShort = 3;
+        myDouble = 4.876;
+        myFloat = 4.3f;
+        myChar = 'e';
+        myBool = false;
+        myLong = 7000;
+        myString = "808 & heartbreak";
+
+	// write your code here
+    }
+}
